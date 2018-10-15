4628889dfbba3118f16625ef9dbe3ecd5c969f94
Diogo Bernardelli <diogobernardelli@jera.com.br>
2018-10-14 21:37:37 -0400

    feat: Change Shelf Feature
    body: Finishing the shelf update feature
    
    - Perfectly updating book shelf - in home/index and also in search
    - Using a Loading component when user change the shelf

--- src/App.js
+++ src/App.js
@@ -3,36 +3,52 @@ import SearchBook from './SearchBook';
 import ListBooks from './ListBooks';
 import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
+import Loading from './Loading';
 import './App.css';
 
 class BooksApp extends React.Component {
   state = {
-    myBooks: []
+    myBooks: [],
+    loading: false
   }
 
   changeShelf = (book, shelf) => {
+    this.setState({
+      loading: true
+    })
+
+    /*
+      I could do another solution here, but it demands
+      a lot of code, which is: after the response from
+      update promise, find the book in myBook state and
+      updating its shelf. If the book isn't in the myBook
+      state, use the get method from API. But it is, at least,
+      10 to 15 plus lines of code..
+    */
+   
     BooksAPI.update(book, shelf).then(() => {
-      let myBooksReplica = this.state.myBooks;
-      myBooksReplica.map(filteredBook => (
-        filteredBook.id === book.id ? filteredBook.shelf = shelf : myBooksReplica
-      ))
-      console.log(myBooksReplica)
-      this.setState({
-        myBooks: myBooksReplica
-      })
+      BooksAPI.getAll().then(myBooks => this.setState({
+        myBooks,
+        loading: false
+      }))
     })
   }
 
   componentDidMount() {
+    this.setState({
+      loading: true
+    })
 		BooksAPI.getAll().then(myBooks => this.setState({
-			myBooks
+			myBooks,
+      loading: false
     }))
 	}
 
   render() {
-    const { myBooks } = this.state;
+    const { myBooks, loading } = this.state;
     return (
       <div className="app">
+        {loading && <Loading /> }
         <Route path='/search' render={() => (
           <SearchBook
             myBooks={myBooks} 
